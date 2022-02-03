23c07ec36b04522c7cf4b7ea1513759b59d719cf
Connor Finch <fuxkvalve@email.com>
2022-02-02 23:59:26 -0800

    init chessML.py

---
+++ chessML.py
@@ -0,0 +1,24 @@
+"""
+Websites for Chess Data:
+    https://www.ficsgames.org/
+    http://database.chessbase.com/
+    https://www.kaggle.com/datasnaek/chess
+    https://www.kaggle.com/milesh1/35-million-chess-games
+
+Other:
+    https://lczero.org/
+    https://github.com/mrklees/deepconv-chess
+    https://github.com/geochri/AlphaZero_Chess
+
+"""
+
+
+
+
+def main():
+    print("inside main")
+
+
+
+if __name__ == "__main__":
+    main()
