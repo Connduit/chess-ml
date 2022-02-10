7a1bdbb92493e2354b2af4244100229e2799e18c
connor <cfinch@uoregon.edu>
2022-02-09 17:13:11 -0800

    init

--- chessML.py
+++ chessML.py
@@ -9,16 +9,28 @@ Other:
     https://lczero.org/
     https://github.com/mrklees/deepconv-chess
     https://github.com/geochri/AlphaZero_Chess
+    https://python-chess.readthedocs.io/en/latest/
 
 """
+import chess
+import chess.engine
+import os
+import asyncio
 
 
 
 
-def main():
-    print("inside main")
+async def main():
 
+    transport, engine = await chess.engine.SimpleEngine.popen_uci(f"{os.getcwd()}/stockfish_14.1_win_x64_avx2.exe")
+    board = chess.Board()
 
+    while not board.is_game_over():
+        result = await engine.play(board, chess.engine.Limit(time=0.1))
+        board.push(result.move)
+    
+    await engine.quit()
 
-if __name__ == "__main__":
-    main()
+asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
+asyncio.run(main())
+#main()
